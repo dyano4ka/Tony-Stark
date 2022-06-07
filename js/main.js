70a1e1fa44c971d59eb2dcc286dc4cf0eb558123
Diana Ch <stamster.c@gmail.com>
2022-06-07 22:21:19 +0300

    new contact form modal

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function showContactModal() {
+    document.getElementById("kontakt").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById('kontakt').style.display = 'none';
+}
